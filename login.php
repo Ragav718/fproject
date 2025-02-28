4bb0e2ea6f635c962503da6f4fa0b4ee18e20408
Ragav718 <ragavan.m.d.718@gmail.com>
2025-02-28 12:20:11 +0530

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,6 @@
 include "config.php";
 session_start();
 
-<center>
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = trim($_POST["email"]);
     $password = $_POST["password"];
@@ -26,7 +25,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Invalid email or password!";
     }
     $stmt->close();
-    </center>
 }
 ?>
 
